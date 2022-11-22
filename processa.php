49d86ca76285ddc1c6515f4b326ff82f79657329
Flavio Francisco da Silva <111377254+Flavio-Francisco@users.noreply.github.com>
2022-11-22 12:59:26 -0300

    atualizado

--- processa.php
+++ processa.php
@@ -27,15 +27,19 @@ function inposto($valor){
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <title>resultado</title>
 </head>
 <body>
     <h1>Resultado</h1>
-    <div>
+    <div class="btm position-absolute top-50 start-50 translate-middle">
         <h5>Seu Salario é: </h5>
-        <label for=""><?php echo $valor; ?></label> <br><br>
+        <label for="" class="primary"><?php echo $valor; ?></label> <br><br>
         <h5>Esse Será o Inposto que Você ira Pagar:</h5>
-        <label for=""><?php inposto($valor) ?></label>
+        <label for="" clas=""><?php inposto($valor) ?></label>
     </div>
 </body>
+  <!-- JavaScript Bundle with Popper -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </html>
